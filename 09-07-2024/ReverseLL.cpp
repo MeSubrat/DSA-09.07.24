f6774e346bfe1e18d1bd689a2a08ff2e7ab83720
Subrat kumar Sahoo <113829258+MeSubrat@users.noreply.github.com>
2024-07-09 22:25:15 +0530

    Create ReverseLL.cpp

---
+++ 09-07-2024/ReverseLL.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* temp=head;
+        ListNode* prev=NULL;
+        while(temp)
+        {
+            ListNode* front=temp->next;
+            temp->next=prev;
+            prev=temp;
+            temp=front;
+        }
+        return prev;
+    }
+};
